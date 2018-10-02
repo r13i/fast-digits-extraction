46a2a4a37666da325e5d7bdc7ff81a159e498d79
Redouane Achouri <achouri.a.r@gmail.com>
2018-10-02 11:39:24 +0100

    Ran final tests to find best results

--- exec_time.py
+++ exec_time.py
@@ -1,14 +1,44 @@
 from time import time
+from numpy import mean
 
-def exec_time(func):
+# def exec_time(func, n_iter=1):
+#     """
+#     Decorator to measure te average execution time of functions
+#     """
+#     def wrapper(*args, **kw):
+#         iteration_times = []
+#         for _ in range(n_iter):
+#             tic = time()
+#             res = func(*args, **kw)
+#             toc = time()
+#             iteration_times.append(toc - tic)
+#         avg_exec_time = mean(iteration_times)
+
+#         print("*** Average Execution of {} :".format(func.__name__))
+#         print("> The result : {}".format(res))
+#         print("> Execution time : {} ms".format(avg_exec_time * 1000), end="\n\n")
+#     return wrapper
+
+
+class exec_time(object):
     """
-    Decorator to measure execution time of functions
+    Decorator to measure te average execution time of functions.
+    @Note This class doesn't start with upper case for the sake of simplicity and readbility
     """
-    def wrapper(*args, **kw):
-        tic = time()
-        res = func(*args, **kw)
-        toc = time()
-        print("*** Execution of {} :".format(func.__name__))
-        print("> The result : {}".format(res))
-        print("> Execution time : {} ms".format((toc - tic) * 1000), end="\n\n")
-    return wrapper
+    def __init__(self, n_iter=1):
+        self.n_iter = n_iter
+
+    def __call__(self, func):
+        def wrapper(*args, **kw):
+            iteration_times = []
+            for _ in range(self.n_iter):
+                tic = time()
+                res = func(*args, **kw)
+                toc = time()
+                iteration_times.append(toc - tic)
+            avg_exec_time = mean(iteration_times)
+
+            print("*** Execution of {} :".format(func.__name__))
+            print("> The result : {}".format(res))
+            print("> Average execution time for {} iterations: {} ms".format(self.n_iter, avg_exec_time * 1000), end="\n\n")
+        return wrapper
